24e1fb15c92d76f8794577d716fa9d1085f360c6
dedonopradonodedelivery-cloud <dedonopradonodedelivery@gmail.com>
2025-11-23 15:39:43 -0300

    index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "./lib/authContext"; // 👈 IMPORTANTE
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
